2ce58f2005826b292b849fde0f727c9fd474ebab
scheckmedia <tobias@scheck-media.de>
2013-10-04 13:33:38 +0200

    update initialization

--- CameraControllerApi/main.cpp
+++ CameraControllerApi/main.cpp
@@ -7,12 +7,12 @@
 //
 
 #include <iostream>
+#include "Settings.h"
 #include "Server.h"
 #include "CameraController.h"
 #include "Api.h"
 
 using namespace CameraControllerApi;
-
 Server *srv;
 
 void sighandler(int sig){
@@ -25,11 +25,22 @@ int main(int argc, const char * argv[])
         printf("%s PORT\n", argv[0]);
         exit(1);
     }
+
+    try {
+        string port;
+        Settings *cfg = Settings::getInstance();
+        bool ret = cfg->get_value("server.port", port);
+        
+        if(ret){
+            int http_port = atoi(port.c_str());
+            srv = new Server(http_port);
+            signal(SIGTERM, sighandler);
+        }
+    } catch (std::exception const &e) {
+        std::cout<<"Error: " << e.what();
+    }
     
-    signal(SIGTERM, sighandler);
     
-    int http_port = atoi(argv[1]);
-    srv = new Server(http_port);
     return 0;
 }
 
