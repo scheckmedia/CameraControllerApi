e97199314d61daf84c8e850d49dc1196b1a78fa8
scheckmedia <tobias@scheck-media.de>
2013-10-09 10:54:50 +0200

    add the following features: set aperture, speed, iso, whitebalance and a method to call the autofocus of the camera

--- CameraControllerApi/Command.cpp
+++ CameraControllerApi/Command.cpp
@@ -18,35 +18,30 @@ using namespace CameraControllerApi;
 
 struct validate_data {
     string api;
-    set<string>* params;
+    string action;
 };
 
 Command::Command(Api *api){
     this->_api = api;
     set<string> params;
-    string param_camera_settings[] = {"list", "aperture", "speed", "iso", "white_balance","focus_point","focus_mode"};
-    string param_execute[] = {"shot", "bulb", "time_lapse","burst", "live"};
+    string param_camera_settings[] = {"list", "aperture", "speed", "iso", "whitebalance","focus_point","focus_mode"};
+    string param_execute[] = {"shot", "bulb", "time_lapse","autofocus", "manualfocus", "live"};
+    string param_files[] = {"list", "get", "delete"};
     _valid_commands["/settings"] = set<string>(param_camera_settings, param_camera_settings + 7);
-    _valid_commands["/capture"] = set<string>(param_execute, param_execute + 5);
+    _valid_commands["/capture"] = set<string>(param_execute, param_execute + 6);
+    _valid_commands["/fs"] = set<string>(param_files, param_execute + 3);
 }
 
 int Command::execute(const string &url, const map<string, string> &argvals, string &response){
-    CCA_API_RESPONSE resp;
+    string param;
     CCA_API_OUTPUT_TYPE type = CCA_OUTPUT_TYPE_JSON;
     validate_data vdata;
     vdata.api = url;
-    vector<string> params;
-    set<string> uniqueparams;
     map<string,string>::const_iterator iterator = argvals.find("action");
     
     if(iterator != argvals.end()){
-        string param = iterator->second;
+        param = iterator->second;
         boost::trim(param);
-        boost::split(params, param, boost::is_any_of(","));
-    }
-    
-    BOOST_FOREACH(string pr, params ) {
-        uniqueparams.insert(pr);
     }
     
     iterator = argvals.find("type");
@@ -56,79 +51,67 @@ int Command::execute(const string &url, const map<string, string> &argvals, stri
             type = CCA_OUTPUT_TYPE_XML;
     }
 
-    vdata.params = &uniqueparams;
+    vdata.action = param;
         
-    if ( !this->_validate(&vdata) || uniqueparams.size() < 1) {
-        resp = CCA_API_RESPONSE_INVALID;
+    if ( !this->_validate(&vdata) || param.empty()) {
         ptree p;
-        Api::buildResponse(p, type, resp, response);
-        return resp;
+        CCA_API_RESPONSE ret = CCA_API_RESPONSE_INVALID;
+        Api::buildResponse(p, type, ret, response);
+        return ret;
     }    
     
-    return this->_executeAPI(url, uniqueparams, argvals, type, response);
+    return this->_executeAPI(url, param, argvals, type, response);
 }
 
-bool Command::_executeAPI(const string &url, const set<string> &actions, const map<string, string> &urlparams, CCA_API_OUTPUT_TYPE type, string &response){
+bool Command::_executeAPI(const string &url, string action, const map<string, string> &urlparams, CCA_API_OUTPUT_TYPE type, string &response){
     bool ret = CCA_CMD_SUCCESS;
+
+    
+    string value;
+    map<string,string>::const_iterator iterator = urlparams.find("value");
+    
+    if(iterator != urlparams.end()){
+        value = iterator->second;
+        boost::trim(value);
+    }
     
     if(url == "/settings"){
-        if(actions.find("list") != actions.end()){
+        if(action.compare("list") == 0){
             ret = this->_api->list_settings(type, response);
-        } else if(actions.find("focus_point") != actions.end()){
-            
-            map<string,string>::const_iterator iterator = urlparams.find("point");
-            if(iterator == urlparams.end()){
-                return false;
-            }
-            
-            string param = iterator->second;
-            boost::trim(param);
-            ret = this->_api->set_focus_point(param, type, response);
+        } else if(action.compare("focus_point") == 0){
+            ret = this->_api->set_focus_point(value, type, response);
+        } else if(action.compare("aperture") == 0){
+            ret = this->_api->set_aperture(value, type, response);
+        } else if(action.compare("speed") == 0){
+            ret = this->_api->set_speed(value, type, response);
+        } else if(action.compare("iso") == 0){
+            ret = this->_api->set_iso(value, type, response);
+        } else if(action.compare("whitebalance") == 0){
+            ret = this->_api->set_whitebalance(value, type, response);
         }
+        
     } else if(url == "/capture"){
-        if(actions.find("shot") != actions.end()){
-            ret = this->_api->shot(type, response);
-        } else if(actions.find("burst") != actions.end()){
-            map<string,string>::const_iterator iterator = urlparams.find("photos");
-            if(iterator == urlparams.end()){
-                return false;
-            }
-            
-            string param = iterator->second;
-            boost::trim(param);
-            ret = this->_api->burst(atoi(param.c_str()), type, response);
-        } else if(actions.find("live") != actions.end()){
-            map<string,string>::const_iterator iterator = urlparams.find("todo");
-            if(iterator == urlparams.end()){
-                return false;
-            }
-            
-            string param = iterator->second;
-            boost::trim(param);
-            if(param.compare("start") == 0)
+        if(action.compare("shot") == 0){
+           ret = this->_api->shot(type, response); 
+        } else if(action.compare("live") == 0){
+            if(value.compare("start") == 0)
                 ret = this->_api->liveview(CCA_API_LIVEVIEW_START, type, response);
             else
                 ret = this->_api->liveview(CCA_API_LIVEVIEW_STOP, type, response);
-
+        } else if(action.compare("autofocus") == 0){
+            ret = this->_api->autofocus(type, response);
         }
+        
     }
     return ret;
 }
 
 bool Command::_validate(const void *data){
     const validate_data *vdata = static_cast<const validate_data *>(data);
-    map<string, set<string>>::iterator it = this->_valid_commands.find(vdata->api);
+    map<string, set<string> >::iterator it = this->_valid_commands.find(vdata->api);
     
     if(it == this->_valid_commands.end()){
         return false;
-    }
-    
-    set<string>::iterator param_it = vdata->params->begin();
-    while(param_it != vdata->params->end()){
-        if(it->second.find(*param_it) == it->second.end())
-            return false;
-        
-        ++param_it;
     }
     
     return true;
